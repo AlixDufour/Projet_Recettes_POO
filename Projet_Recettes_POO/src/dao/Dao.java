99a28d737c4c29a9cedbbb36d1da13465c89cec2
BOUGUET Theo <theo.bougue@gmail.com>
2022-11-08 23:35:45 -0500

    Ajout méthodes interface Dao

--- Projet_Recettes_POO/src/dao/Dao.java
+++ Projet_Recettes_POO/src/dao/Dao.java
@@ -1,5 +1,15 @@
 package dao;
 
+import java.util.List;
+
 public interface Dao<T> {
 
+	List<T> getAll();
+	
+	void create(T t);
+	
+	void update(T t, String[] params);
+	
+	void delete(T t);
+	
 }
