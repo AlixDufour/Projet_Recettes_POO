b2d2e83f8812b1089a842339a9c68005d5d1f4a8
Oceane-cn <61272371+Oceane-cn@users.noreply.github.com>
2022-12-06 14:57:43 -0500

    [VUES] Création de la vue accueil

--- Projet_Recettes_POO/src/application/Main.java
+++ Projet_Recettes_POO/src/application/Main.java
@@ -1,8 +1,14 @@
 package application;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+
+import java.awt.ScrollPane;
+
 import dao.Dao;
 import dao.RecetteDAO;
 
@@ -13,8 +19,8 @@ public class Main extends Application {
 		RecetteDAO test = new RecetteDAO();
 		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("./../scenes/MainScene.fxml"));
+			Scene scene = new Scene(root,800,450);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
