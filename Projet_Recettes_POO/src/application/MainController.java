5303bb8286338afe210f613da8d1bffe7eb41d7c
Oceane-cn <61272371+Oceane-cn@users.noreply.github.com>
2022-12-09 15:15:52 -0500

    Correction de l'affichage dynamique des recettes sur la page d'accueil

--- Projet_Recettes_POO/src/application/MainController.java
+++ Projet_Recettes_POO/src/application/MainController.java
@@ -7,9 +7,14 @@ import java.util.ResourceBundle;
 import dao.RecetteDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MainController implements Observateur, Initializable {
 
@@ -17,22 +22,49 @@ public class MainController implements Observateur, Initializable {
 	private RecetteDAO recettes;
 
 	@FXML
-	private HBox listeRecettes;
+	private GridPane listeRecettes;
 	@FXML
 	private ImageView profileButton;
+	
+	@FXML 
+	private Button returnButton;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		listeRecettes.getChildren().clear();
 		recettes = new RecetteDAO();
+		int maxColumnNumber = 4;
+		int x = 1;
+		int y = 1;
 		for (Recette r : recettes.getAll()) {
+			VBox vbox = new VBox();
 			Label label = new Label(r.getName());
-			listeRecettes.getChildren().add(label);
+			
+			// Mise en forme de la vbox
+			vbox.getChildren().add(label);
+			vbox.setPadding(new Insets(30, 30, 30, 30));
+			vbox.setAlignment(Pos.CENTER); 
+			
+			listeRecettes.add(vbox, x, y);
+			x++;
+			// Si x dépasse le nombre de colonnes alors on passe à la ligne suivante
+			if(x > maxColumnNumber) {
+				x = 1;
+				y++;
+			}
 		}
 
 		profileButton.setOnMouseClicked(e -> {
 			try {
 				model.switchScene(CreationScenes.creerProfileScene(model));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		});
+		
+		returnButton.setOnMouseClicked(e -> {
+			try {
+				model.switchScene(CreationScenes.creerChoixProfileScene(model));
 			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
