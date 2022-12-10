25704c43df936e7fc48689a8739bb5df01038e76
Oceane-cn <61272371+Oceane-cn@users.noreply.github.com>
2022-12-10 11:25:13 -0500

    [RECETTE FORM] Correction de l'ajout de champ pour les régimes

--- Projet_Recettes_POO/src/application/RecetteFormController.java
+++ Projet_Recettes_POO/src/application/RecetteFormController.java
@@ -13,16 +13,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
 
 public class RecetteFormController implements Initializable, Observateur {
 
 	private Modele model;
-	
-	@FXML
-	private Button enregistrerRecette, retourAccueil, ajouterIngrédient, ajouterUstensile, ajouterEtape, ajouterRegime;
 	
 	@FXML
 	private TextField form_nom, form_duree, form_desc, form_prix;
@@ -33,78 +32,24 @@ public class RecetteFormController implements Initializable, Observateur {
 	@FXML
 	private VBox ingredientsBox, ustensilesBox, etapesBox, regimeBox;
 	
+	private int countEtapes;
+	
 	@Override
 	public void reagir() {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {		
+	public void initialize(URL arg0, ResourceBundle arg1) {	
+		countEtapes = 1;
+		
 		ObservableList<String> niveauChoices = FXCollections.observableArrayList("Facile", "Intermédiaire", "Difficile"); ;
 		form_diff.setItems(niveauChoices);
 		form_diff.setValue("Facile");
 		
 		ObservableList<String> regimesChoices = FXCollections.observableArrayList("Classique", "Végétarien", "Végétalien"); ;
 		form_regime.setItems(regimesChoices);
-		form_regime.setValue("Classique");	
-		
-		ajouterIngrédient.setOnMouseClicked(e -> {
-			try {
-				HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormIngredient.fxml"));
-				ingredientsBox.getChildren().add((Node) form);
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		});
-		
-		ajouterUstensile.setOnMouseClicked(e -> {
-			try {
-				HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormUstensile.fxml"));
-				ustensilesBox.getChildren().add((Node) form);
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		});
-		
-		ajouterEtape.setOnMouseClicked(e -> {
-			try {
-				HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormEtape.fxml"));
-				etapesBox.getChildren().add((Node) form);
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		});
-		
-		ajouterRegime.setOnMouseClicked(e -> {
-			try {
-				HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormRegime.fxml"));
-				regimeBox.getChildren().add((Node) form);
-			} catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		});
-		
-		enregistrerRecette.setOnMouseClicked(e -> {
-			if(saveRecette()) {
-				try {
-					model.switchScene(CreationScenes.creerMainScene(model));
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-			else {
-				// TODO Afficher message d'erreur
-			}
-		});
-		
-		retourAccueil.setOnMouseClicked(e -> {
-			try {
-				model.switchScene(CreationScenes.creerMainScene(model));
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		});
-
+		form_regime.setValue("Classique");
 	}
 
 	public void setModele(Modele m) {
@@ -134,4 +79,76 @@ public class RecetteFormController implements Initializable, Observateur {
 				List<Regime> regimes);*/
 		return true;
 	}
+	
+	@FXML
+	public void ajouterLigneIngredient(ActionEvent event) {
+		try {
+			HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormIngredient.fxml"));
+			ingredientsBox.getChildren().add((Node) form);
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void ajouterLigneUstensile(ActionEvent event) {
+			try {
+				HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormUstensile.fxml"));
+				ustensilesBox.getChildren().add((Node) form);
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+	}
+	
+	@FXML
+	public void ajouterLigneEtape(ActionEvent event) {
+		try {
+			countEtapes ++;
+			HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormEtape.fxml"));
+			((Label) form.getChildren().get(0)).setText("Etape n°" + countEtapes);
+			etapesBox.getChildren().add((Node) form);
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void ajouterLigneRegime(ActionEvent event) {
+		try {
+			HBox form = FXMLLoader.load(getClass().getResource("./../scenes/FormRegime.fxml"));
+			ObservableList<String> regimesChoices = FXCollections.observableArrayList("Classique", "Végétarien", "Végétalien");
+			System.out.print(form.getChildren());
+			((ChoiceBox) form.getChildren().get(1)).setItems(regimesChoices);
+			regimeBox.getChildren().add((Node) form);
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void retourAccueil(ActionEvent event) {
+			try {
+				model.switchScene(CreationScenes.creerMainScene(model));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+	}
+	
+	@FXML
+	public void enregistrerRecette(ActionEvent event) {
+			if(saveRecette()) {
+				try {
+					model.switchScene(CreationScenes.creerMainScene(model));
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+			else {
+				// TODO Afficher message d'erreur
+			}
+	}
+	
+	
+	
+	
 }
